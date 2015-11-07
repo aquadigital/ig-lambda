48a94dc15f8205d6bd6014a43d792c1cb68c0868
Nick Ethan <nick@igniter.io>
2015-11-07 14:58:45 -0700

    final tweaks for content sending and tied into intercom.

--- ig-content/gruntfile.js
+++ ig-content/gruntfile.js
@@ -8,7 +8,7 @@ grunt.initConfig({
   },
   lambda_deploy: {
     default: {
-      arn: 'arn:aws:lambda:us-east-1:123456781234:function:my-function'
+      arn: 'arn:aws:lambda:us-west-2:731103058072:function:igScheduleContent'
     }
   },
   lambda_package: {
--- ig-content/index.js
+++ ig-content/index.js
@@ -2,46 +2,108 @@ var request = require('request');
 var Intercom = require('intercom-client');
 var Promise = require('bluebird');
 var _ = require('underscore');
+var moment = require('moment');
+var config = {
+  testEvent: 'http://api2.socialmention.com/search?q=iphone+apps&f=json&t=blogs&lang=en',
+  intercomID: 'dmqf1z3p',
+  intercomKey: '692af63d625d711408ec565f121720c99bb6f6ed',
+  intercomClient: {}
+}
 
 exports.handler = function (event, context) {
-  console.log('Querying SocialMention = ' + event.webpage);
-  exports.getContent(event).then(function(content) {
-    var client = new Intercom.Client('dmqf1z3p', '692af63d625d711408ec565f121720c99bb6f6ed');
-    var message = {
-      message_type: "email",
-      subject: "Here is some awesome content!",
-      body: content,
-      template: "plain",
-      from: {
-        type: "admin",
-        id: "77811"
-      },
-      to: {
-        type: "user",
-        id: "554e4b2585fc0947e600c68b"
-      }
-    }
+  var script = {
+    client: {},
+    segmentId: '563e604c1d0a4489dc0000a6',
+    init: function() {
+      var self = this;
 
-    client.messages.create(message, function() {
-      context.done(null, 'content-notification complete.');
-    });
-  });
-};
-
-exports.getContent = function(event) {
-  return new Promise(function (resolve, reject) {
-    request(event.webpage, function (err, response, body) {
-      if (err) console.log(err, err.stack);
-      var content = '';
-      var items = JSON.parse(body).items;
-
-      items = _.first(items, 10);
-
-      _.each(items, function(item) {
-        content += '<h1>' + item.title + '</h1>';
+      this.client = new Intercom.Client(config.intercomID, config.intercomKey);
+      this.getUsers().then(function(users) {
+        Promise.map(users, function(user) {
+          return self.getContent(user);
+        })
+        .each(function(result) {
+          return self.sendMessage(result.user, result.content);
+        })
+        .then(function() {
+          context.done(null, 'content-notification complete.');
+        });
       });
+    },
+    getContent: function(user) {
+      var self = this;
+      return new Promise(function (resolve, reject) {
+        request(config.testEvent, function (err, response, body) {
+          if (err) console.log(err, err.stack);
 
-      resolve(content);
-    });
-  });
+          var content = '';
+          var items = JSON.parse(body).items;
+
+          items = _.first(items, 10);
+
+          _.each(items, function(item) {
+            content += '<h1>' + item.title + '</h1>';
+          });
+
+          var result = {
+            user: user,
+            content: content
+          }
+
+          resolve(result);
+        });
+      });
+    },
+    getUsers: function() {
+      var self = this;
+      return new Promise(function (resolve, reject) {
+        self.client.users.listBy({ segment_id: self.segmentId }, function(result) {
+          if (result.statusCode === 200) {
+            resolve(result.body.users);
+          } else {
+            reject('error talking to intercom.');
+          }
+        });
+      });
+    },
+    sendMessage: function(user, contentResult) {
+      var self = this;
+      return new Promise(function (resolve, reject) {
+        var message = {
+          message_type: "email",
+          subject: "Here is some awesome content!",
+          body: contentResult,
+          template: "plain",
+          from: {
+            type: "admin",
+            id: "77811"
+          },
+          to: {
+            type: "user",
+            id: user.id
+          }
+        }
+
+        self.client.messages.create(message, function() {
+          resolve();
+        });
+      });
+    },
+    updateUserLastSent: function() {
+      var self = this;
+      return new Promise(function (resolve, reject) {
+        var postData = {
+          id: '554e4b2585fc0947e600c68b',
+          custom_attributes: {
+            'last-suggested-content': moment().format()
+          }
+        }
+        config.intercomClient.users.create(postData, function (r) {
+          resolve();
+        });
+      });
+    }
+  }
+
+  script.init();
 };
